f633baf1ac48890ebe33a8111a7d71246cc9bfa8
Hrithic <hrithicraj730@gmail.com>
2025-03-02 05:06:56 +0530

    google auth fixed

--- src/components/GoogleAuthButton.tsx
+++ src/components/GoogleAuthButton.tsx
@@ -1,38 +1,43 @@
 import React from 'react'
-import { googleAuth } from '../redux/slices/authSlice'
+// import { googleAuth } from '../redux/slices/authSlice'
 import Glogo from '../assets/images/GoogleLogo.png';
-import { useAppDispatch, useAppSelector } from '../hooks/reduxHooks';
+import { useAppSelector } from '../hooks/reduxHooks';
 import { useNavigate } from 'react-router-dom';
-
+import { useGoogleLogin } from '@react-oauth/google';
+import axiosInstance from '../utils/axiosInstance';
 
 const GoogleAuthButton: React.FC = () => {
-    const dispatch = useAppDispatch();
+    // const dispatch = useAppDispatch();
     const navigate = useNavigate();
     const {googleLoading} = useAppSelector((state)=> state.auth);
-    const handleGoogleAuth =()=>{
-        // window.location.href = "http://localhost:5000/api/auth/google";
-        dispatch(googleAuth())
-        .unwrap()
-        .then((response) => {
-            console.log("Google Auth successful:", response);
-            navigate('/home');
-        })
-        .catch((err) => {
-            console.error("Google Auth failed:", err);
-        });
-    }
+    const handleGoogleAuth =useGoogleLogin({
+        onSuccess: async(credentialResponse)=>{
+            try{
+                const res = await axiosInstance.post('/auth/google',{credentialResponse});
+                const {accessToken} = res.data;
+                localStorage.setItem('token', accessToken);
+                localStorage.setItem('role', res.data.role);
+                if(res.data.newUser){
+                    navigate('/select-languages');
+                }else{
+                    navigate('/');
+                }
+            }catch(error){
+                console.error("Google auth failed", error);
+            }
+        },
+        onError: () => console.log("Google auth failed"),
+    })
   return (
     <div className='flex flex-col justify-center items-center'>
         <button
-            onClick={handleGoogleAuth}
-            disabled={googleLoading}
+            onClick={()=>handleGoogleAuth()}
+            // disabled={googleLoading}
             className='w-[311px] h-[43px] flex justify-center items-center space-x-3 rounded-[15px] border-2 border-[#46cec2]'
         >
             <img className='w-8 h-[31px] rounded-full' src={Glogo} alt="" />
             <span className="text-[#46cec2] text-xl font-normal font-['Geologica']">{googleLoading ? "Loading..." : "Continue with Google"}</span>
         </button>
-        {/* {googleError && <span className="text-red-500">{googleError}</span>} */}
-        {/* {user && <span className="text-green-500">Welcome, {user.name}!</span>} */}
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -7,13 +7,18 @@ import { Provider } from 'react-redux'
 import store from './redux/store.ts'
 import { persistor } from './redux/persistor.ts'
 import { PersistGate } from 'redux-persist/integration/react'
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
+const CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID ?? "";
+console.log(CLIENT_ID)
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-          <App />
+          <GoogleOAuthProvider clientId={CLIENT_ID}>
+            <App />
+          </GoogleOAuthProvider>
         </PersistGate>
       </Provider>
     </BrowserRouter>

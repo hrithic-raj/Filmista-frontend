73f5d0dbd8fbe05c09a74030199f540de4f81407
Hrithic <hrithicraj730@gmail.com>
2025-03-03 15:28:12 +0530

    adding PWA in filmista

--- src/main.tsx
+++ src/main.tsx
@@ -8,8 +8,18 @@ import store from './redux/store.ts'
 import { persistor } from './redux/persistor.ts'
 import { PersistGate } from 'redux-persist/integration/react'
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { registerSW } from "virtual:pwa-register";
 
 const CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID ?? "";
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("New version available. Reload?")) {
+      updateSW(true);
+    }
+  },
+});
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
